2e6ae3509aac3a37b19969286468063fe15dc318
ranjith2007 <ranjithkumar0799@gmail.com>
2025-11-28 12:11:43 +0530

    k8-eks

---
+++ 00-vpc/vpc.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source                = "git::https://github.com/ranjith2007/terraform-aws-vpc.git?ref=main"
+  vpc_cidr              = var.vpc_cidr
+  project_name          = var.project_name
+  environment           = var.environment
+  common_tags           = var.comman_tags
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
+  database_subnet_cidrs = var.database_subnet_cidrs
+  is_peering_required   = var.is_peering_required
+}
